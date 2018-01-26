73faf7f9cbfaab26678b986d88da34ef9a241268
Japorized <johnson5756@live.com>
2018-01-25 20:30:35 -0500

    Adding personal bookmarks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp'),
       sass = require('gulp-sass'),
+      uglify = require('gulp-uglify'),
+      pump = require('pump'),
       gulpUtil = require('gulp-util');
 
 gulp.task('sass', () => {
@@ -11,3 +13,13 @@ gulp.task('sass', () => {
 gulp.task('sass:watch', () => {
   gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('uglify', (cb) => {
+  pump([
+      gulp.src('./src/js/**/*.js'),
+      uglify(),
+      gulp.dest('./assets/js')
+    ],
+    cb
+  );
+})
---
+++ src/js/functionality.js
@@ -0,0 +1,30 @@
+var btns = document.querySelectorAll('#right > div'),
+	queryBar = document.querySelector('#left input');
+
+for (var i = 0; i < btns.length; i++) {
+	btns[i].addEventListener('click', function(e) {
+		document.querySelector('#right > .active').classList.remove('active');
+		this.classList += 'active';
+		document.querySelector('#left > .active').classList.remove('active');
+		document.querySelector('#left > #' + this.getAttribute('id') + '-content').classList += 'active';
+	}, false);
+}
+
+document.onkeydown = function (e) {
+	if (e.shiftKey && e.keyCode == 49) {
+		var curActive = document.querySelector('#right > .active');
+		if ( curActive.getAttribute('id') !== 'search' ) {
+			curActive.classList.remove('active');
+			document.querySelector('#right > #search').classList += 'active';
+			document.querySelector('#left > .active').classList.remove('active');
+			document.querySelector('#left > #search-content').classList += 'active';
+		}
+		queryBar.focus();
+	}
+};
+
+queryBar.onkeydown = function(e) {
+	if (e.key == "Enter") {
+		window.location.href = "https://duckduckgo.com/?q=" + this.value;
+	}
+};
