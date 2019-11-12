c810c82e575717aaf660bc4c6d3731aa31b32abe
Japorized <johnson5756@live.com>
2019-11-12 06:01:59 -0500

    add DDG bang completion

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,51 @@
-const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      uglify = require('gulp-uglify'),
-      pump = require('pump'),
-      browserSync = require('browser-sync');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const browserSync = require('browser-sync');
+const babel = require('gulp-babel');
 
-gulp.task('sass', (done) => {
-  gulp.src('./src/sass/**/*.scss')
+gulp.task('sass', done => {
+  gulp
+    .src('./src/sass/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'));
   done();
 });
 
-gulp.task('uglify', (cb) => {
+gulp.task('uglify', () => {
   try {
-    pump([
-        gulp.src('./src/js/**/*.js'),
-        uglify({
-          compress: {
-            collapse_vars: true,
-            toplevel: true
-          }
-        }),
-        gulp.dest('./assets/js')
-      ],
-      cb
-    );
+    return gulp
+      .src('./src/js/**/*.js')
+      .pipe(babel())
+      .pipe(uglify())
+      .pipe(gulp.dest('./assets/js'));
   } catch (e) {
-   console.log('gulp uglify: ' + e);
+    console.log('gulp uglify: ' + e);
   }
 });
 
-gulp.task('browser-sync', gulp.series(['sass', 'uglify'], (done) => {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    }
-  });
-  gulp.watch(['./index.html', './assets/**/*'], cb => {
-    browserSync.reload();
-    cb();
-  });
-  done();
-}));
+gulp.task(
+  'browser-sync',
+  gulp.series(['sass', 'uglify'], done => {
+    browserSync.init({
+      server: {
+        baseDir: './',
+      },
+    });
+    gulp.watch(['./index.html', './assets/**/*'], cb => {
+      browserSync.reload();
+      cb();
+    });
+    done();
+  }),
+);
 
 gulp.task('watcher', () => {
-  gulp.watch(['./src/sass/**/*.scss'], gulp.series( 'sass' ));
-  gulp.watch(['./src/js/**/*.js'], gulp.series( 'uglify' ));
+  gulp.watch(['./src/sass/**/*.scss'], gulp.series('sass'));
+  gulp.watch(['./src/js/**/*.js'], gulp.series('uglify'));
 });
 
 gulp.task('build', gulp.series(['sass', 'uglify']));
-gulp.task('watch', gulp.series( ['browser-sync', 'watcher'] ));
-gulp.task('default', gulp.series( [ 'watch' ] ));
+gulp.task('watch', gulp.series(['browser-sync', 'watcher']));
+gulp.task('default', gulp.series(['watch']));
--- src/js/functionality.js
+++ src/js/functionality.js
@@ -2,10 +2,56 @@
 var btns = document.querySelectorAll('#right > div'),
 	queryBar = document.querySelector('#left input');
 
-window.data = getJSON('/elementary/bookmarks.json');
+let bangs = [
+  { "bang": "apkg", "name": "Archlinux packages" },
+  { "bang": "archpkg", "name": "Archlinux packages" },
+  { "bang": "arch", "name": "Archlinux wiki" },
+  { "bang": "archwiki", "name": "Archlinux wiki" },
+  { "bang": "archaur", "name": "Archlinux AUR" },
+  { "bang": "aur", "name": "Archlinux AUR" },
+  { "bang": "archlinux", "name": "Archlinux Forum" },
+  { "bang": "archforum", "name": "Archlinux Forum" },
+  { "bang": "w", "name": "Wikipedia" },
+  { "bang": "wiki", "name": "Wikipedia" },
+  { "bang": "bulba", "name": "Bulbapedia" },
+  { "bang": "yt", "name": "YouTube" },
+  { "bang": "youtube", "name": "YouTube" },
+  { "bang": "ytor", "name": "YouTube on Repeat" },
+  { "bang": "youtubeonrepeat", "name": "YouTube on Repeat" },
+  { "bang": "gh", "name": "Github" },
+  { "bang": "github", "name": "Github" },
+  { "bang": "glab", "name": "Gitlab" },
+  { "bang": "gitlab", "name": "Gitlab" },
+  { "bang": "latexwb", "name": "LaTeX Wikibooks" },
+  { "bang": "ctan", "name": "CTAN" },
+  { "bang": "crates", "name": "Crates.io" },
+  { "bang": "cargo", "name": "Crates.io" },
+  { "bang": "rust", "name": "Rust stdlib docs" },
+  { "bang": "rustdoc", "name": "Rust docs" },
+  { "bang": "js", "name": "MDN JS Documentation" },
+  { "bang": "css", "name": "MDN CSS Documentation" },
+  { "bang": "csst", "name": "CSS Tricks" },
+  { "bang": "r", "name": "Reddit" },
+  { "bang": "subr", "name": "Subreddits" },
+  { "bang": "subreddit", "name": "Subreddits" },
+  { "bang": "dictionaryr", "name": "Dictionary.com (reference)" },
+  { "bang": "thes", "name": "Thesaurus.com" },
+  { "bang": "thesaurus", "name": "Thesaurus.com" },
+  { "bang": "wa", "name": "Wolfram Alpha" },
+  { "bang": "wolfram", "name": "Wolfram Alpha" },
+  { "bang": "wolf", "name": "Wolfram Alpha" },
+  { "bang": "math", "name": "Wolfram Mathworld" },
+  { "bang": "mathworld", "name": "Wolfram Mathworld" }
+];
+
+if ( window.location.href.includes("http://localhost") ) {
+  window.data = getJSON('/bookmarks.json');
+} else {
+  window.data = getJSON('/elementary/bookmarks.json');
+}
 
 for (var i = 0; i < btns.length; i++) {
-	btns[i].addEventListener('mouseover', function(e) {
+	btns[i].addEventListener('mouseover', function() {
 		document.querySelector('#right > .active').classList.remove('active');
 		this.classList += 'active';
 		document.querySelector('#left > .active').classList.remove('active');
@@ -30,6 +76,7 @@ document.onkeyup = function (e) {
 // smart search
 queryBar.onkeydown = function(e) {
   var $this = this;
+
 	if (e.key == "Enter") {
 		if (hasProtocol($this.value)) {
 			  window.location.href = $this.value;
@@ -50,11 +97,11 @@ queryBar.onkeydown = function(e) {
         }
 		  }
 		}
-	} else if (e.keyCode == 9) {
+	} else if (e.keyCode == 9) { // tab key
 		e.preventDefault();
 		if ($this.value == "ht") {
 			$this.value = 'https://';
-		} else {
+    } else {
       window.data.then(function(database) {
         for ( var data in database ) {
           data = database[data];
@@ -69,6 +116,21 @@ queryBar.onkeydown = function(e) {
     }
 	}
 };
+
+queryBar.onkeyup = function(e) {
+  if ( this.value === "" ) {
+    cleanBangMatches();
+  } else if ( this.value.charAt(0) === "!" ) {
+    const matches = checkbang(this.value.substring(1));
+    showBangMatches(matches);
+    if ( matches.length === 0 ) {
+      cleanBangMatches();
+    }
+    if ( e.keyCode === 9 ) {
+      this.value = "!" + matches[0].bang;
+    }
+  }
+}
 
 // fill in bookmarks onload
 function bookmarks() {
@@ -163,20 +225,89 @@ function hasProtocol(url) {
 
 function getJSON(path) {
   return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", path, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
+    const sessionKey = path.split('/')[1];
+    if ( window.sessionStorage.getItem(sessionKey) === null ) {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", path, true);
+      xhr.responseType = 'json';
+      xhr.onload = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                resolve(xhr.response);
-            } else {
-              reject(xhr.status);
-            }
+          if (xhr.status === 200) {
+            window.sessionStorage.setItem(sessionKey, JSON.stringify(xhr.response));
+            resolve(xhr.response);
+          } else {
+            reject(xhr.status);
+          }
         }
-    };
-    xhr.send();
+      };
+      xhr.send();
+    } else {
+      const json = JSON.parse(window.sessionStorage.getItem(sessionKey));
+      resolve(json);
+    }
   });
+}
+
+// DISABLED and kept for reference
+// REASON: CORS disallow reading of resource from different origin
+// SOLUTION: Keep a personal list of bangs
+// function getDDGBangs() {
+//   const bangsSrc = "https://duckduckgo.com/bang.v253.js";
+//   return new Promise(function(resolve, reject) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("GET", bangsSrc, true);;
+//     xhr.send();
+//     xhr.onload = () => {
+//       if (xhr.readyState === XMLHttpRequest.DONE) {
+//         if (xhr.status === 200) {
+//           window.localStorage("bang", xhr.response);
+//           resolve(xhr.response);
+//         } else {
+//           reject(xhr.status);
+//         }
+//       }
+//     };
+//   });
+// }
+
+function checkbang(query) {
+  return bangs.filter(item => item.bang.includes(query));
+}
+
+function showBangMatches(matches) {
+  const searchWrapper = document.querySelector('#left > #search-content');
+  let div = document.querySelector('.bang-matches');
+  if ( div === null ) {
+    div = document.createElement("div");
+    div.classList.add('bang-matches');
+    searchWrapper.appendChild(div);
+  }
+  let ul = document.querySelector('.bang-matches ul');
+  if ( ul === null ) {
+    ul = document.createElement("ul");
+    div.appendChild(ul);
+  } else {
+    while (ul.hasChildNodes()) {
+      ul.removeChild(ul.lastChild);
+    }
+  }
+  matches.forEach((match) => {
+    const bang = document.createElement("b");
+    bang.innerHTML = "!" + match.bang;
+    const desc = document.createElement("span");
+    desc.innerHTML = match.name;
+    const li = document.createElement("li");
+    li.appendChild(bang);
+    li.appendChild(desc);
+    ul.appendChild(li);
+  });
+}
+
+function cleanBangMatches() {
+  const div = document.querySelector('.bang-matches');
+  if ( div !== null ) {
+    div.parentNode.removeChild(div);
+  }
 }
 
 bookmarks();
